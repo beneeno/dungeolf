ece63ee5c3a42552b0bf747eab159c9b23bec17d
beansmyth <63074515+ben0x0a@users.noreply.github.com>
2022-08-01 15:05:18 -0400

    Tilemap changes

--- Data/Scripts/Block.gd
+++ Data/Scripts/Block.gd
@@ -6,15 +6,17 @@ var roughness
 func _ready():
 	pass
 
-func init(id):
-	# Set traits
-	match id:
-		0: # Normal
-			bounciness = 0.5
-			roughness = 0.01
-		1: # Bouncy
-			bounciness = 0.8
-			roughness = 0.01
-		2: # Rough
-			bounciness = 0.25
-			roughness = 0.1
+func init(b, r):
+	bounciness = b
+	roughness = r
+#	# Set traits
+#	match id:
+#		0: # Normal
+#			bounciness = 0.5
+#			roughness = 0.01
+#		1: # Bouncy
+#			bounciness = 0.8
+#			roughness = 0.01
+#		2: # Rough
+#			bounciness = 0.25
+#			roughness = 0.1
--- Data/Scripts/TileMapMain.gd
+++ Data/Scripts/TileMapMain.gd
@@ -1,13 +1,30 @@
 extends TileMap
 
-export var Block: PackedScene
+const GRASS = 0
+const BRICK = 1
+
+const bounciness = {
+	GRASS: 0.5,
+	BRICK: 0.8
+}
+const roughness = {
+	GRASS: 0.01,
+	BRICK: 0.01
+}
+
+export var Tile: PackedScene
 
 func _ready():
-	var blocks = get_used_cells()
-	for i in range(0, blocks.size()):
-		var pos = map_to_world(blocks[i])
-		var b = Block.instance()
-		var id = get_cell(blocks[i].x, blocks[i].y)
-		b.init(id)
-		b.position = pos + Vector2(5,5)
-		get_parent().call_deferred("add_child", b)
+	print(bounciness)
+	var tiles = get_used_cells()
+	for i in tiles.size():
+		var pos = map_to_world(tiles[i])
+		var id = get_cell(tiles[i].x, tiles[i].y)
+		
+		# Instance based on tile id
+		match id:
+			GRASS, BRICK:
+				var t = Tile.instance()
+				t.init(bounciness[id], roughness[id])
+				t.position = pos + Vector2(5, 5)
+				get_parent().call_deferred("add_child", t)
