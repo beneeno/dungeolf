9be6dab17c5b148620a759cb9df827bd06a2efe4
beansmyth <63074515+ben0x0a@users.noreply.github.com>
2022-08-02 10:01:26 -0400

    Main menu
    Start of menu implementation

---
+++ Data/Scripts/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_PlayButton_pressed():
+	if not $AnimationPlayer.is_playing():
+		$AnimationPlayer.play("Levels_Open")
+
+func _on_OptionsButton_pressed():
+	pass # Replace with function body.
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+func _on_LevelsBackButton_pressed():
+	if not $AnimationPlayer.is_playing():
+		$AnimationPlayer.play("Levels_Close")
