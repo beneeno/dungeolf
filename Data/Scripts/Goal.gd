8107f53ecd8cbd32233ad0dddc54fd015781a028
beansmyth <63074515+ben0x0a@users.noreply.github.com>
2022-08-04 16:39:20 -0400

    Ball and hole improvements
    Made the ball look like it goes inside the hole via some z index stuff

--- Data/Scripts/Goal.gd
+++ Data/Scripts/Goal.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+var _ball_in_area = false
+var _win = false
+
+onready var area = $Sprite/Area2D
+
 func _ready():
 	pass
+	
+func _physics_process(_delta):
+	if _ball_in_area:
+		if position.distance_to(get_node("../Ball").position) < 3:
+#			get_parent().level_complete()
+			get_node("../Ball").z_index = -1
+			_win = true
 
 func _on_Area2D_body_entered(body):
-	body.global_position = global_position
 	if body.get_name() == "Ball":
-		get_parent().level_complete()
+		_ball_in_area = true
+
+func _on_Area2D_body_exited(body):
+	if body.get_name() == "Ball":
+		_ball_in_area = false
+		
+		if _win == true:
+			get_node("../Ball").queue_free()
